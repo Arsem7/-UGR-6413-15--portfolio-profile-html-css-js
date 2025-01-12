44e3c8bf7103e9c3a21b0cfaba3a49bd2a276243
Arsem7 <167558219+Arsem7@users.noreply.github.com>
2025-01-12 20:07:04 +0300

    Delete My Portfolio directory

--- My Portfolio/Javascript/script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const nav = document.querySelector("nav");
-  menuToggle.onclick = () => {
-    nav.classList.toggle("nav");
-  };
-});
