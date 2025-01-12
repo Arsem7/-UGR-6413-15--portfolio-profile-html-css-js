ac9726a05c6a2df5316ac9425842bad8f73532b3
Arsem7 <167558219+Arsem7@users.noreply.github.com>
2025-01-12 18:58:24 +0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,22 +1,4 @@
-// // script.js
-// document.addEventListener('DOMContentLoaded', () => {
-//   const navbarLinks = document.querySelectorAll('.navbar a');
-
-//   navbarLinks.forEach(link => {
-//       link.addEventListener('click', e => {
-//           e.preventDefault();
-//           const targetId = e.target.getAttribute('href').substring(1);
-//           const targetSection = document.getElementById(targetId);
-
-//           window.scrollTo({
-//               top: targetSection.offsetTop - 50,
-//               behavior: 'smooth',
-//           });
-//       });
-//   });
-// });
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
+entListener('DOMContentLoaded', () => {
   const menuToggle = document.getElementById("menu-toggle");
   const nav = document.querySelector("nav");
   menuToggle.onclick = () => {
