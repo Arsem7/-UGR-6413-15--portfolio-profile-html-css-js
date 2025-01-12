3d1e7324a650da86f3b818d55edb83b8c19603fc
Arsem7 <167558219+Arsem7@users.noreply.github.com>
2025-01-12 19:44:38 +0300

    Delete Javascript directory

--- Javascript/script.js
+++
@@ -1,7 +0,0 @@
-entListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const nav = document.querySelector("nav");
-  menuToggle.onclick = () => {
-    nav.classList.toggle("nav");
-  };
-});
